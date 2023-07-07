dd67e2db69cdad308909fcedefd09d9027f2fe0a
kenser <koffy140221@gmail.com>
2023-07-06 20:14:17 -0600

    cambios de variables con diferentes tipos de datos

---
+++ script.js
@@ -0,0 +1,25 @@
+//variables tipo numero 
+if (true) {
+    var m = 105;
+}
+console.log(m);
+let numero = 15 
+ //variable tipo string 
+ let nombre = "mi nombre es kenser omar"
+
+ let apellidos = 'caal juc'
+
+ // tipo booleano
+
+ let valor1 = true
+
+ let valor2 = false
+
+ // dato nulo
+
+ let dato1 = null
+ let dato2 = null
+
+
+ //objetos 
+ 
