2db2fdf74f87cd6896a15cfe363c1c30a0a2365b
kenser <koffy140221@gmail.com>
2023-07-06 19:32:45 -0600

    crear html para la tarea

---
+++ tarea1.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TAREA NO.1</title>
+</head>
+<body>
+    <h1>TAREA NO. 1 SUBTTE CAAL INTRODUCCION A JS</h1>
+    <script src="./script.js"></script>
+</body>
+</html>
